eb386eec3821b66798d90b6aa91a82d5f1075afa
Dongsusin <auroratime020715@gmail.com>
2025-09-10 00:01:18 +0900

    빌드값 수정

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import PokedexGallery from "./components/PokedexGallery";
+function App() {
+    return _jsx(PokedexGallery, {});
+}
+export default App;
